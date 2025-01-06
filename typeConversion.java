43404d5f0d6aa8fb3516636148f863bcb80331ee
Prince Vaviya <1princevaviya@gmail.com>
2025-01-06 22:36:09 +0530

    java

---
+++ typeConversion.java
@@ -0,0 +1,11 @@
+class typeConversion{
+    public static void main(String[] args){
+        int i = 200;
+        long l = i;
+        float f = l;
+
+        System.out.println("int value : " + i);
+        System.out.println("long value : " + l);
+        System.out.println("float value : " + f);
+    }
+}
