a50752e310d3ae39a2cc6c9e8c0c56f5541fb431
Prince Vaviya <1princevaviya@gmail.com>
2025-01-08 22:38:38 +0530

    CompoundOperators

---
+++ CompoundOperators.java
@@ -0,0 +1,11 @@
+class CompoundOperators{
+    public static void main(String args[]){
+        int num1 = 101;
+        System.out.println("Addition Assignment Operator : " + (num1 += 1));
+        System.out.println("Subtraction Assignment Operator : " + (num1 -= 11));
+        System.out.println("Multiplication Assignment Operator : " + (num1 *= 111));
+        System.out.println("Division Assignment Operator : " + (num1 /= 10));
+        System.out.println("Modulus Assignment Operator : " + (num1 %= 101));
+
+    }
+}
