c5e89f3c4370a731143c28d422421f0efa945536
Prince Vaviya <1princevaviya@gmail.com>
2025-01-24 13:16:05 +0530

    StudentClass

--- StudentClass.java
+++ StudentClass.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Student{
     int roll_no;
     double marks1, marks2;
@@ -16,21 +18,28 @@ class Student{
 
 class StudentClass{
     public static void main(String[] args){
+
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.print("Enter the name of the student : ");
+        String name = myObj.next();
+
+        System.out.print("Enter the roll no. of the student : ");
+        int roll_no = myObj.nextInt();
+
+        System.out.print("Enter the marks1 of the student : ");
+        double marks1 = myObj.nextDouble();
+
+        System.out.print("Enter the marks2 of the student : ");
+        double marks2 = myObj.nextDouble();
+
         Student s1 = new Student(); 
-        s1.name = "Mr.Eagle";
-        s1.marks1 = 70;
-        s1.marks2 = 71;
-        s1.roll_no = 005;
-        System.out.println("Total marks achieved by : " + s1.name + " is " + s1.CalculateTotal());
+        s1.name = name;
+        s1.marks1 = marks1;
+        s1.marks2 = marks2;
+        s1.roll_no = roll_no;
+        System.out.println("\nTotal marks achieved by : " + s1.name + " is " + s1.CalculateTotal());
         s1.PrintResult();
         System.out.println();
-
-        Student s2 = new Student(); 
-        s2.name = "Unknown";
-        s2.marks1 = 0;
-        s2.marks2 = 1;
-        s2.roll_no = 001;
-        System.out.println("Total marks achieved : " + s2.name + " is " + s2.CalculateTotal());
-        s2.PrintResult();
     }
 }
