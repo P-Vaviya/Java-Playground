1c0ec2c91f81c8cbf4208cb8272d8d563cd30fb1
Prince Vaviya <1princevaviya@gmail.com>
2025-01-29 11:17:50 +0530

    Object Class toString Method

--- Class.java
+++ Class.java
@@ -1,4 +1,3 @@
-import java.lang.Object;
 
 class Class{
     int id = 005, roll = 4005;
@@ -6,12 +5,8 @@ class Class{
     public String toString(){
         return ("id : "+id+" name : "+name+" roll : "+roll);
     }
-
-
-
     public static void main(String[] args){
         Class c = new Class();
         System.out.println(c);
     }
-
 }
